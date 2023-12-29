a67eb2f55a3a553b95cb6f0fd6e3757f3c932114
QuentinHsu <xuquentinyang@gmail.com>
2023-12-29 22:35:34 +0800

    perf: format code

--- src/views/home/components/header/index.vue
+++ src/views/home/components/header/index.vue
@@ -1,44 +1,47 @@
 <script setup lang="ts">
-import { watch } from "vue";
-import { useColorMode, UseColorModeReturn } from "@vueuse/core";
-import { useRouter } from "vue-router";
-const router = useRouter();
-const systemThemeMode: UseColorModeReturn = useColorMode(); // Ref<'dark' | 'light'>
+import { watch } from 'vue'
+import type { UseColorModeReturn } from '@vueuse/core'
+import { useColorMode } from '@vueuse/core'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const systemThemeMode: UseColorModeReturn = useColorMode() // Ref<'dark' | 'light'>
 
-const themeMode: UseColorModeReturn = systemThemeMode; // Ref<'dark' | 'light'>
+const themeMode: UseColorModeReturn = systemThemeMode // Ref<'dark' | 'light'>
 
 watch(
   () => themeMode.value,
   (value) => {
     switch (value) {
-      case "dark":
-        document.documentElement.setAttribute("theme-mode", "dark");
-        break;
-      case "light":
-        document.documentElement.removeAttribute("theme-mode");
-        break;
+      case 'dark':
+        document.documentElement.setAttribute('theme-mode', 'dark')
+        break
+      case 'light':
+        document.documentElement.removeAttribute('theme-mode')
+        break
 
       default:
-        break;
+        break
     }
   },
-  { immediate: true }
-);
+  { immediate: true },
+)
 </script>
 
 <template>
   <t-head-menu class="header">
     <template #logo>
-      <div class="logo" @click="router.push({ path: '/' })">oh dev</div>
+      <div class="logo" @click="router.push({ path: '/' })">
+        oh dev
+      </div>
     </template>
     <template #operations>
       <t-radio-group v-model:value="themeMode">
-        <t-radio-button value="light"
-          ><t-icon name="mode-light"
-        /></t-radio-button>
-        <t-radio-button value="dark"
-          ><t-icon name="mode-dark"
-        /></t-radio-button>
+        <t-radio-button value="light">
+          <t-icon name="mode-light" />
+        </t-radio-button>
+        <t-radio-button value="dark">
+          <t-icon name="mode-dark" />
+        </t-radio-button>
       </t-radio-group>
     </template>
   </t-head-menu>
