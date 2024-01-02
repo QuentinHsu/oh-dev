adeffbe3462be1dccdc3d652d3a1d20b1b581df2
QuentinHsu <xuquentinyang@gmail.com>
2024-01-02 21:17:46 +0800

    perf: resolve alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { TDesignResolver } from "unplugin-vue-components/resolvers";
-import path from "node:path";
-
-const alias = {
-  "@": path.join(__dirname, "src"),
-};
+import { URL, fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { TDesignResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,18 +12,21 @@ export default defineConfig({
     AutoImport({
       resolvers: [
         TDesignResolver({
-          library: "vue-next",
+          library: 'vue-next',
         }),
       ],
     }),
     Components({
       resolvers: [
         TDesignResolver({
-          library: "vue-next",
+          library: 'vue-next',
         }),
       ],
     }),
   ],
-  resolve: { alias },
-  base: "",
-});
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
