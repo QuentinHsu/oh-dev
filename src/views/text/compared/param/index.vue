e8d516a836b2024373c13f9008e3b8b5850d6575
QuentinHsu <xuquentinyang@gmail.com>
2024-02-29 15:03:17 +0800

    style: code format

--- src/views/text/compared/param/index.vue
+++ src/views/text/compared/param/index.vue
@@ -1,46 +1,53 @@
 <script setup lang="ts">
-import { type Ref, ref } from 'vue'
-import Enter from './components/enter.vue'
-import EnterKeys from './components/enter-keys.vue'
+import type { Ref } from "vue";
+import { ref } from "vue";
 
-const enterValue: Ref<{ [key: string]: any }> = ref({})
-const enterKeysValue: Ref<string[]> = ref([])
+import EnterKeys from "./components/enter-keys.vue";
+import Enter from "./components/enter.vue";
+
+const enterValue: Ref<Record<string, any>> = ref({});
+const enterKeysValue: Ref<string[]> = ref([]);
 </script>
 
 <template>
-  <t-space style="width: 100%;" direction="vertical" size="large">
-    <t-row>
-      <t-descriptions
-        item-layout="vertical"
-        size="large" style="width: 100%" :label-style="{ padding: '0 0 0 16px' }"
-        class="content-text"
-      >
-        <t-descriptions-item label="Object">
-          <Enter v-model:value="enterValue" />
-        </t-descriptions-item>
-        <t-descriptions-item label="Keys">
-          <EnterKeys v-model:value="enterKeysValue" />
-        </t-descriptions-item>
-      </t-descriptions>
-      <div v-if="Object.keys(enterValue).length > 0" class="">
-        <t-descriptions
-          size="large" style="width: 100%" :label-style="{ width: '100px', padding: '0 0 0 16px' }"
-        >
-          <t-descriptions-item label="Object.keys">
-            {{ Object.keys(enterValue).length }}
-          </t-descriptions-item>
-        </t-descriptions>
-      </div>
-    </t-row>
-  </t-space>
+	<TSpace direction="vertical" size="large">
+		<TRow>
+			<TDescriptions
+				class="content-text"
+				item-layout="vertical"
+				:label-style="{ padding: '0 0 0 16px' }"
+				size="large"
+			>
+				<TDescriptionsItem label="Object">
+					<Enter v-model:value="enterValue" />
+				</TDescriptionsItem>
+				<TDescriptionsItem label="Keys">
+					<EnterKeys v-model:value="enterKeysValue" />
+				</TDescriptionsItem>
+			</TDescriptions>
+			<div v-if="Object.keys(enterValue).length > 0" class="">
+				<TDescriptions
+					:label-style="{ width: '100px', padding: '0 0 0 16px' }"
+					size="large"
+				>
+					<TDescriptionsItem label="Object.keys">
+						{{ Object.keys(enterValue).length }}
+					</TDescriptionsItem>
+				</TDescriptions>
+			</div>
+		</TRow>
+	</TSpace>
 </template>
 
 <style lang="less" scoped>
+.t-space,
+.t-descriptions,
 :deep(.t-descriptions__body) {
-  width: 100%
+	width: 100%;
 }
-:deep(.t-descriptions__content ) {
-  box-sizing: border-box;
-  height: 100%;
+
+:deep(.t-descriptions__content) {
+	box-sizing: border-box;
+	height: 100%;
 }
 </style>
