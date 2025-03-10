75fc69d9f7066e0f71806a7da1c2ed3f5085375e
QuentinHsu <xuquentinyang@gmail.com>
2025-03-10 22:23:41 +0800

    style(route): standardize import order and improve code formatting

--- src/views/text/route.ts
+++ src/views/text/route.ts
@@ -1,12 +1,12 @@
-import URL from './url/route'
-import HTML from './html/route'
-import JSON from './json/route'
-import Compared from './compared/route'
+import Compared from "./compared/route";
+import HTML from "./html/route";
+import JSON from "./json/route";
+import URL from "./url/route";
 
 export default [
-  {
-    path: 'text',
-    meta: { title: '' },
-    children: [...URL, ...HTML, ...JSON, ...Compared],
-  },
-]
+	{
+		path: "text",
+		meta: { title: "" },
+		children: [...URL, ...HTML, ...JSON, ...Compared],
+	},
+];
