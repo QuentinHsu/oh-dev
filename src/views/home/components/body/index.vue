dcbb1a1f4af7f0579e45db68beea3ba0edd5bc6a
QuentinHsu <xuquentinyang@gmail.com>
2025-03-10 21:47:03 +0800

    refactor(global): improve code formatting and structure across multiple files

--- src/views/home/components/body/index.vue
+++ src/views/home/components/body/index.vue
@@ -1,44 +1,49 @@
 <script setup lang="ts">
 interface menuItem {
-  path: string
-  name: string
-  icon: string
+	path: string;
+	name: string;
+	icon: string;
 }
-const menuList: Array<menuItem> = [
-  { path: '/text/url/analyze', name: 'URL 解析', icon: 'app' },
-  { path: '/text/html/entity', name: 'HTML 字符转义', icon: 'app' },
-  { path: '/text/json', name: 'JSON', icon: 'app' },
-  { path: '/text/compared', name: '对比', icon: 'app' },
-]
+const menuList: menuItem[] = [
+	{ path: "/text/url/analyze", name: "URL 解析", icon: "app" },
+	// { path: "/text/html/entity", name: "HTML 字符转义", icon: "app" },
+	// { path: "/text/json", name: "JSON", icon: "app" },
+	// { path: "/text/compared", name: "对比", icon: "app" },
+];
 </script>
 
 <template>
-  <div class="body">
-    <t-layout>
-      <t-aside>
-        <t-menu class="menu-right">
-          <t-menu-item v-for="item in menuList" :key="item.path" :value="item.path" :to="{ path: item.path }">
-            <template #icon>
-              <t-icon :name="item.icon" />
-            </template>
-            {{ item.name }}
-          </t-menu-item>
-        </t-menu>
-      </t-aside>
-      <t-layout>
-        <div class="container">
-          <RouterView />
-        </div>
-      </t-layout>
-    </t-layout>
-  </div>
+	<div class="body">
+		<TLayout>
+			<TAside>
+				<TMenu class="menu-right">
+					<TMenuItem
+						v-for="item in menuList"
+						:key="item.path"
+						:to="{ path: item.path }"
+						:value="item.path"
+					>
+						<template #icon>
+							<TIcon :name="item.icon" />
+						</template>
+						{{ item.name }}
+					</TMenuItem>
+				</TMenu>
+			</TAside>
+			<TLayout>
+				<div class="container">
+					<RouterView />
+				</div>
+			</TLayout>
+		</TLayout>
+	</div>
 </template>
 
 <style lang="less" scoped>
 .body {
-  .container {
-    padding: 20px;
-    box-shadow: var(--td-shadow-inset-left);
-  }
+	.container {
+		padding: 20px;
+		box-shadow: var(--td-shadow-inset-left);
+	}
 }
 </style>
--- src/views/text/url/analyze/components/input.vue
+++ src/views/text/url/analyze/components/input.vue
@@ -1,64 +1,78 @@
 <script setup lang="ts">
-import { reactive } from 'vue'
-import type { TypeTextURLAnalyze } from '../type'
-import { parseURL } from '../utils'
-import { REG_EXP_URL } from '@/constants/regex.ts'
+import { reactive } from "vue";
 
-const emits = defineEmits([
-  'update:modelValue',
-])
+import { REG_EXP_URL } from "@/constants/regex.ts";
+
+import type { TypeTextURLAnalyze } from "../type";
+import { parseURL } from "../utils";
+
+const emit = defineEmits(["update:modelValue"]);
+
 const form = reactive({
-  inputRaw: '',
-})
-const inputStatus: TypeTextURLAnalyze['inputStatus'] = reactive({
-  isHttpURL: 'default',
-})
-function validatorInput(value: string): boolean | object {
-  const infoURL = parseURL(form.inputRaw)
-  if (!value) {
-    inputStatus.isHttpURL = 'default'
-    emits('update:modelValue', infoURL)
-    return { result: true, message: '', type: 'success' }
-  }
-  else if (REG_EXP_URL.isHttpURL.test(value)) {
-    inputStatus.isHttpURL = 'success'
-    emits('update:modelValue', infoURL)
-    return { result: true, message: '太强了，你输入了个有效的 HTTP URL！', type: 'success' }
-  }
-  else {
-    inputStatus.isHttpURL = 'error'
-    emits('update:modelValue', infoURL)
-    return { result: false, message: '不太对劲儿啊！我觉得它不是一个有效的 HTTP URL！', type: 'error' }
-  }
+	inputRaw: "",
+});
+
+const inputStatus: TypeTextURLAnalyze["inputStatus"] = reactive({
+	isHttpURL: "default",
+});
+
+function validatorInput(value: string): boolean | Record<PropertyKey, unknown> {
+	const infoURL = parseURL(form.inputRaw);
+
+	if (!value) {
+		inputStatus.isHttpURL = "default";
+		emit("update:modelValue", infoURL);
+
+		return { result: true, message: "", type: "success" };
+	} else if (REG_EXP_URL.isHttpURL.test(value)) {
+		inputStatus.isHttpURL = "success";
+		emit("update:modelValue", infoURL);
+
+		return {
+			result: true,
+			message: "太强了，你输入了个有效的 HTTP URL！",
+			type: "success",
+		};
+	} else {
+		inputStatus.isHttpURL = "error";
+		emit("update:modelValue", infoURL);
+
+		return {
+			result: false,
+			message: "不太对劲儿啊！我觉得它不是一个有效的 HTTP URL！",
+			type: "error",
+		};
+	}
 }
+
 /**
  * 清空输入
  */
 function onClear(): void {
-  form.inputRaw = ''
+	form.inputRaw = "";
 }
+
 const formRules = {
-  inputRaw: [
-    { validator: validatorInput },
-  ],
-}
+	inputRaw: [{ validator: validatorInput }],
+};
+
 defineExpose({
-  onClear,
-})
+	onClear,
+});
 </script>
 
 <template>
-  <t-form label-width="0" :data="form" :rules="formRules">
-    <t-form-item name="inputRaw">
-      <t-textarea
-        v-model="form.inputRaw"
-        placeholder="请输入有效的 HTTP URL 内容"
-        :autosize="{ minRows: 5 }"
-        :status="inputStatus.isHttpURL"
-        class="input-textarea"
-      />
-    </t-form-item>
-  </t-form>
+	<TForm :data="form" label-width="0" :rules="formRules">
+		<TFormItem name="inputRaw">
+			<TTextarea
+				v-model="form.inputRaw"
+				:autosize="{ minRows: 5 }"
+				class="input-textarea"
+				placeholder="请输入有效的 HTTP URL 内容"
+				:status="inputStatus.isHttpURL"
+			/>
+		</TFormItem>
+	</TForm>
 </template>
 
 <style lang="less" scoped></style>
